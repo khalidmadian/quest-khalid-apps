92dffcc9bf889f78735a8799bfda11040294d4ef
khalidmadian <khalidabolftoh@gmail.com>
2024-12-20 08:49:10 +0200

    Resolved merge conflicts

--- custom_addons/todo_app/models/todo_task.py
+++ custom_addons/todo_app/models/todo_task.py
@@ -8,8 +8,8 @@ class TodoTask(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(srtring='Name')
-    due_date = fields.Date(string='Due Date' ,tracking=1)
-    description = fields.Text(srtring='Description')
+    due_date = fields.Date(string='Due Date', tracking=1)
+    description = fields.Text(srtring='Description', tracking=1)
     # (tracking=1) to make this fiels display on the chatter
     assign_to = fields.Many2one('res.partner', string='Assign To', tracking=1)
     status = fields.Selection([
